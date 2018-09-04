70a0013215dbf3e495887f08d1ad9d3d150e327d
Mehdi <m.mahdi.gh1997@gmail.com>
2018-09-04 20:00:03 +0430

    version 1.0

--- setserver.sh
+++ setserver.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Set Your server name or ip here.
-openconnect de.cisadd2.com
+openconnect DOMAIN OR IP
 # IMPOETANT : DO NOT CHANGE this lines!
-#echo "[sudo] password for mmg:"
-#read ANSWER
 echo "Enter 'yes' to accept, 'no' to abort; anything else to view:"
 read ANSWER
 echo "Usrename:"
