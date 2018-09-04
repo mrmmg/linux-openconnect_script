cf0f17f727e46f9369fa57008ad93fb1908d1dd6
Mehdi <m.mahdi.gh1997@gmail.com>
2018-09-04 19:58:44 +0430

    version 1.0

--- openconnect.sh
+++ openconnect.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/expect
 spawn ./setserver.sh
-#expect exact "[sudo] password for mmg:\r"
-#send "toor\r"
 expect exact "Enter 'yes' to accept, 'no' to abort; anything else to view:\r"
 send "yes\r"
 expect exact "Usrename:\r"
-send "ir388476\r"
+#set your username here!
+send "USERNAME\r"
 expect exact "Password:\r"
-send "mrmmg127\r"
+#set your password here!
+send "PASSWORD\r"
 expect eof
 sleep 1000000
-
